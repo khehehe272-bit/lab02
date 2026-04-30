c42692e3cff71b1703e37ced759385eced9e41a9
khehehe272-bit <khehehe272@gmail.com>
2026-04-30 10:29:48 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,1 @@
-#include <iostream>\n#include <string>\n\nint main() {\n    std::string name;\n    std::cout << "Enter your name: ";\n    std::cin >> name;\n    std::cout << "Hello world from @" << name << std::endl;\n    return 0;\n}
+#include <iostream>\n#include <string>\n\nint main() {\n    std::string name;\n    std::cout << "Введите ваше имя: ";\n    std::cin >> name;\n    std::cout << "Hello world from @" << name << std::endl;\n    return 0;\n}
